f24f575745ab9bfba66de7c8244b187c539321cb
Yuki Takahashi <ytnspuv0105@gmail.com>
2026-05-02 14:59:59 +0900

    登録機能

--- app/routers/transaction.py
+++ app/routers/transaction.py
@@ -3,27 +3,42 @@ from app.schemas.transaction import TransactionCreate
 
 router = APIRouter()
 
-@router.get("/transactions")
+# 👇 追加（メモリ保存）
+transactions = [
+    {
+        "id": 1,
+        "title": "ランチ",
+        "amount": 1000,
+        "type": "expense",
+        "date": "2026-05-01",
+        "category_id": 1
+    },
+    {
+        "id": 2,
+        "title": "給料",
+        "amount": 300000,
+        "type": "income",
+        "date": "2026-05-01",
+        "category_id": 1
+    }
+]
 
+@router.get("/transactions")
 def get_transactions():
     return {
-        "data": [
-            {
-                "id": 1,
-                "title": "ランチ",
-                "amount": 1000,
-                "type": "expense"
-            },
-            {
-                "id": 2,
-                "title": "給料",
-                "amount": 300000,
-                "type": "income"
-            }
-        ],
+        "data": transactions,
         "meta": {}
     }
 
 @router.post("/transactions")
 def create_transaction(data: TransactionCreate):
-    return data
+    new_id = len(transactions) + 1
+
+    new_transaction = {
+        "id": new_id,
+        **data.dict()
+    }
+
+    transactions.append(new_transaction)
+
+    return new_transaction
