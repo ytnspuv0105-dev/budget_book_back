58c376e23d7bc061c06670391b1b0ecbb3ca3620
Yuki Takahashi <ytnspuv0105@gmail.com>
2026-05-03 22:06:27 +0900

    postgeSQL導入

--- app/db.py
+++ app/db.py
@@ -1,10 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "postgresql://yuki@localhost/budget_app"
 
-engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
