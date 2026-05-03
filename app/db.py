3bffa7ca4826ac623e3d909650fe7e57c545005e
Yuki Takahashi <ytnspuv0105@gmail.com>
2026-05-03 22:10:44 +0900

    .envファイル導入

--- app/db.py
+++ app/db.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://yuki@localhost/budget_app"
+# 👇 追加
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
