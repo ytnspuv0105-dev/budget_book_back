df132b7a3980303010a087aa995582fe79becb08
Yuki Takahashi <ytnspuv0105@gmail.com>
2026-08-03 20:42:38 +0900

    フロントエンドURLの読み込み改修

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import transaction, category
 
 app = FastAPI()
 
+origins = ["http://localhost:3000"]
+
+frontend_url = os.getenv("FRONTEND_URL")
+if frontend_url:
+    origins.append(frontend_url.rstrip("/"))
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=origins,
     allow_methods=["*"],
     allow_headers=["*"],
 )
